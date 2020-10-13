687d25fbf24976abd41a2ca7a8159a1fca19ea27
afuwape <giloadewale123@gmail.com>
2020-10-13 02:17:49 +0100

    this is Asterik exercise from chapter six

---
+++ AsterikTest.java
@@ -0,0 +1,15 @@
+package inheritance;
+import java.util.Scanner;
+public class AsterikTest {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the magic number: ");
+		int num = input.nextInt();
+		
+	Asterik.squareOfAsterik(num);
+	
+	}
+}
