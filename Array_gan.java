adcc86801cfe77c77c1f127f4813e49fc3b84fa8
afuwape <giloadewale123@gmail.com>
2020-10-22 04:21:33 +0100

    every possible concept of array

---
+++ Array_gan.java
@@ -0,0 +1,46 @@
+package ganiu_project;
+
+public class Array_gan {
+
+	public static void main(String[] args) {
+		
+		/*  int[] array = {87,68,94,100,83,78,85,91,76,87};
+		
+		int total = 0;
+		
+		for(int counter = 0; counter < array.length; counter++)
+			
+			total += array[counter];
+		
+		System.out.printf("Total of array elements: %d\n", total);  */
+		
+		
+		
+	/*	int[] array  = new int[10];
+		
+		System.out.printf("%s%8s%n", "index", "value");
+		
+		for(int counter = 0; counter < array.length; counter++) {
+			
+			System.out.printf("%5d%8d%n",counter, array[counter]);
+			
+	
+		}*/
+		
+	/*	final int ARRAY_LENGTH = 10;
+		
+		int[] array = new int [ARRAY_LENGTH];
+	
+		for(int counter = 0; counter < array.length; counter++) {
+		
+		array[counter] = 2 + 2 * counter;
+		
+		System.out.printf("%s%8s%n", "index", "value");
+		
+		for(int counter = 0; counter < array.length; counter++);
+		System.out.printf("%5d%8d%n", counter, array[counter]);
+		}*/
+		
+		
+	}
+}
